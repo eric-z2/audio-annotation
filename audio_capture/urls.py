897a08c23c2b2d7e8cab7fdb55f7152ad8129756
Eric Zhang <163900411+eric-z2@users.noreply.github.com>
2026-01-06 09:10:52 -0500

    fixed annotation urls

--- audio_capture/urls.py
+++ audio_capture/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("homepage/", views.homepage, name="homepage"),
     path("annotation/", views.annotation, name="annotation"),
-    path("save_audio/", views.save_audio, name="save_audio")
+    path("save_audio_essay/", views.save_audio_essay, name="save_audio_essay"),
+    path("save_audio_aita/", views.save_audio_aita, name="save_audio_aita"),
 ]
