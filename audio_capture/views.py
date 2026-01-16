ef0985dc760abfbf469dc19619813bf3b766f82b
Eric Zhang <163900411+eric-z2@users.noreply.github.com>
2026-01-16 13:46:55 -0500

    first iteration of changes

--- audio_capture/views.py
+++ audio_capture/views.py
@@ -8,27 +8,30 @@ from .models import EssayAudioStorage, AITAAudioStorage
 import datetime
 
 def homepage(request):
-    if request.method == 'POST' and request.POST.get('consent'):
+    if request.method == 'POST' and request.POST.get('id'):
+        request.session['crowdworker_id'] = request.POST.get('id')
         return redirect('annotation')
     return render(request, 'homepage.html')
 
 def annotation(request):
+    if 'crowdworker_id' not in request.session:
+        return redirect('homepage')
     template = loader.get_template("annotation.html")
     return HttpResponse(template.render())
 
 def save_audio_essay(request):
     template = loader.get_template("homepage.html") # Fix
-    user_id = EssayAudioStorage.objects.all().count() + 1
     
     if request.method == 'POST':
         data = json.loads(request.body)
         audio_data = data.get('audio_base64') 
         date = datetime.datetime.now()
+        crowdworker_id = request.session.get('crowdworker_id', 'unknown')
 
-        audio_file = ContentFile(base64.b64decode(audio_data), name=f"essay_{date}.webm")
+        audio_file = ContentFile(base64.b64decode(audio_data), name=f"{crowdworker_id}_essay_{date}.webm")
 
         new_entry = EssayAudioStorage(
-            user_id=user_id,
+            user_id=crowdworker_id,
             essay_id=data.get('essay_id'),
             audio_file=audio_file
         )
@@ -44,20 +47,22 @@ def save_audio_essay(request):
 
 def save_audio_aita(request):
     template = loader.get_template("homepage.html") # Fix
-    user_id = AITAAudioStorage.objects.all().count() + 1
     
     if request.method == 'POST':
         data = json.loads(request.body)
         audio_data = data.get('audio_base64') 
         date = datetime.datetime.now()
+        crowdworker_id = request.session.get('crowdworker_id', 'unknown')
 
-        audio_file = ContentFile(base64.b64decode(audio_data), name=f"aita_{date}.webm")
+        audio_file = ContentFile(base64.b64decode(audio_data), name=f"{crowdworker_id}_aita_{date}.webm")
 
         new_entry = AITAAudioStorage(
-            user_id=user_id,
+            user_id=crowdworker_id,
             post_id=data.get('aita_id'),
-            audio_file=audio_file
+            audio_file=audio_file,
+            label=data.get('label')
         )
+        print(data.get('label'))
         new_entry.save()
 
         # # Debugging
