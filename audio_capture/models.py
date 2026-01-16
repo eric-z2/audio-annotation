6d689e8c15321ce1f8536aab3308623cd4550c51
Eric Zhang <163900411+eric-z2@users.noreply.github.com>
2026-01-16 13:46:55 -0500

    first iteration of changes

--- audio_capture/models.py
+++ audio_capture/models.py
@@ -8,8 +8,9 @@ class EssayAudioStorage(models.Model):
     audio_file = models.FileField(upload_to='essay/')
     datetime = models.DateTimeField(auto_now=True)
 
-class AITAAudioStorage(models.Model):
+class AITAAudioStorage(models.Model): # Make more general
     user_id = models.IntegerField() # Not sure whether to use int or strings for this field (or if we even want this field)
     post_id = models.IntegerField()
     audio_file = models.FileField(upload_to='aita/')
+    label = models.CharField(max_length=100)
     datetime = models.DateTimeField(auto_now=True)
