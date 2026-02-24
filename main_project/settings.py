c1c0921f67bea741e3f1718ff31e56ea0c857cd9
Charlie Welch <welchc9@cw.cas.mcmaster.ca>
2026-02-24 14:11:05 -0500

    static config

--- main_project/settings.py
+++ main_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'SECRET_KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nlp.cas.mcmaster.ca', '127.0.0.1', '*']
 
 
 # Application definition
@@ -116,7 +116,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = 'audio_capture/static/'
+
+STATIC_URL = 'homepage/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
