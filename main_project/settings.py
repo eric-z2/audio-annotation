a24de6fed1d5feef7887da45b18e9801a4c5dd4e
Eric Zhang <163900411+eric-z2@users.noreply.github.com>
2026-02-27 13:48:45 -0500

    changed url endpoints

--- main_project/settings.py
+++ main_project/settings.py
@@ -118,9 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = 'audio_capture/static/'
-
-STATIC_URL = 'homepage/static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
