4707b8b6accd2996f9bb7c3d9308ecec53ba7241
Eric Zhang <163900411+eric-z2@users.noreply.github.com>
2026-03-23 15:28:23 -0400

    finished ui

--- audio_capture/urls.py
+++ audio_capture/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("audio_collect/", views.homepage, name="homepage"),
-    path("audio_annotation/", views.annotation, name="annotation"),
+    path("annotation/homepage/", views.homepage, name="homepage"),
+    path("annotation/audio_annotation/", views.annotation, name="annotation"),
     path("save_audio_essay/", views.save_audio_essay, name="save_audio_essay"),
     path("save_audio_aita/", views.save_audio_aita, name="save_audio_aita"),
 ]
